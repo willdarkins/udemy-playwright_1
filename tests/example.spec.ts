45c6bc05e9b1203d636c24715cfe35f841f02e3f
Will Darkins <will.darkins@adcellerant.com>
2024-10-05 16:04:44 -0700

    added notes and actually ran test - SUCCESS

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test'
 
 //Telling Playwright to open the browser and load the address in the link
 test('Simple basic test', async ( {page} ) => {
-    await page.goto('https://www.example.com')
-    const pageTitle = await page.locator('h1')
-    await expect(pageTitle).toContainText('Example Domain')
+    await page.goto('https://www.example.com') //First we load the site by using 'goto' method
+    const pageTitle = await page.locator('h1') //Then created variable to store a page element, and pass said element as a value
+    await expect(pageTitle).toContainText('Example Domain') //now we're accessing the element and calling to expect it... then use 'toContainText' method and input the expected value
 })
