064b30367cfced2279f774f2d7029f4ed5e52a4d
Will Darkins <will.darkins@adcellerant.com>
2024-10-08 09:53:56 -0700

    provided configuration settings to file and specified projects to run tests

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,1 +1,31 @@
 import { PlaywrightTestConfig } from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+    timeout: 60000, //miliseconds you want run for a playwright test to finish
+    retries: 0, //How many times you want playwright to rerun failed tests
+    //'use' is for specific settings you want to apply to the environment
+    use: {
+        headless: true, //Do you want to run the tests with, or without opening the browser?
+        viewport: {width: 1280, height: 720}, //what dimension the screen should be in
+        actionTimeout: 15000, //setting the maximum time allowed for an action to complete before it is considered a failure
+        ignoreHTTPSErrors: true,
+        video: 'off',
+        screenshot: 'off',
+    },
+    projects: [
+        {
+            name: 'Chromium',
+            use: {browserName: 'chromium'}
+        },
+        {
+            name: 'Firefox',
+            use: {browserName:'firefox'}
+        },
+        {
+            name: 'Webkit',
+            use: {browserName: 'webkit'}
+        }
+    ]
+}
+
+export default config
