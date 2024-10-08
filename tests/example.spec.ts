711df650a1f3c313ea23d5a72414be8896a14499
Will Darkins <will.darkins@adcellerant.com>
2024-10-07 22:27:17 -0700

    basic annotations and creating test suites

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -19,52 +19,55 @@ test ('Clicking on elements', async ({page})=> {
 })
 
 
-// test ('Selectors Testing', async ({page})=> {
-//     //selecting based on text
-//     await page.click('text=some text')
+test.skip ('Selectors Testing', async ({page})=> {
+    //selecting based on text
+    await page.click('text=some text')
 
-//     //css selectors
-//     await page.click('button')
-//     await page.click('#id')
-//     await page.click('.class')
+    //css selectors
+    await page.click('button')
+    await page.click('#id')
+    await page.click('.class')
 
-//     //only visible css selectors - clicking on something that is only visible on screen
-//     await page.click('.submit-button:visible')
+    //only visible css selectors - clicking on something that is only visible on screen
+    await page.click('.submit-button:visible')
 
-//     //combinations
-//     await page.click('#user-name . first') //clicking on an element with and ID and user name shared
+    //combinations
+    await page.click('#user-name . first') //clicking on an element with and ID and user name shared
 
-//     //xpath - need to ask joe for more reference on this
-//     await page.click('//button')
-// })
+    //xpath - need to ask joe for more reference on this
+    await page.click('//button')
+})
 
-test ('Working with inputs', async ({page})=> {
-    const errorMessage = await page.locator('.alert-error')
+test.describe('My first test suite', () => {
+    test ('Working with inputs', async ({page})=> {
+        const errorMessage = await page.locator('.alert-error')
+        
+        await page.goto('http://zero.webappsecurity.com/index.html')
+        await page.click('#signin_button')
     
-    await page.goto('http://zero.webappsecurity.com/index.html')
-    await page.click('#signin_button')
-
-    await page.fill('#user_login', 'some username')
-    await page.fill('#user_password', 'some password') //in the arguement, the first entry will represent the selector, while the second concerns the text
-    await page.click('text=Sign in')
-    await expect (errorMessage).toContainText('Login and/or password are wrong.'); //error message is throwing and we're expecting the text passed as a value
-
+        await page.fill('#user_login', 'some username')
+        await page.fill('#user_password', 'some password') //in the arguement, the first entry will represent the selector, while the second concerns the text
+        await page.click('text=Sign in')
+        await expect (errorMessage).toContainText('Login and/or password are wrong.'); //error message is throwing and we're expecting the text passed as a value
+    
+    })
+    //asertions bascialy mean we're expecting something about the element to be true
+    test ('working with assertions', async ({page})=> {
+        await page.goto('https://example.com')
+        await expect(page).toHaveURL('https://example.com')
+        await expect(page).toHaveTitle('Example Domain')
+    
+        //making sure the h1 element is correctly displayed on the screen
+        const element = await page.locator('h1')
+        await expect(element).toBeVisible
+        await expect (element).toHaveText('Example Domain')//a more strict assertion that the text listed is in fact part of the h1 element
+        await expect (element).toHaveCount(1)//verifying there is in fact 1 h1 heading on the page
+    
+        //we can also verify certain elements are NOT visisble on the webpage
+        //in this instance we're talking about the h5 heading...
+        const nonExistingElement = await page.locator('h5')
+        await expect (nonExistingElement).not.toBeVisible
+        //*IMPORTANT* when working with an element, it needs to be stored in a varibale before being used in assertion
+    })
 })
-//asertions bascialy mean we're expecting something about the element to be true
-test ('working with assertions', async ({page})=> {
-    await page.goto('https://example.com')
-    await expect(page).toHaveURL('https://example.com')
-    await expect(page).toHaveTitle('Example Domain')
 
-    //making sure the h1 element is correctly displayed on the screen
-    const element = await page.locator('h1')
-    await expect(element).toBeVisible
-    await expect (element).toHaveText('Example Domain')//a more strict assertion that the text listed is in fact part of the h1 element
-    await expect (element).toHaveCount(1)//verifying there is in fact 1 h1 heading on the page
-
-    //we can also verify certain elements are NOT visisble on the webpage
-    //in this instance we're talking about the h5 heading...
-    const nonExistingElement = await page.locator('h5')
-    await expect (nonExistingElement).not.toBeVisible
-    //*IMPORTANT* when working with an element, it needs to be stored in a varibale before being used in assertion
-})
